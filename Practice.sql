88475cff6d58b03676c2cea4b899d2386c0ee48d
KalpeshSinghMeena <kalpeshclg@gmail.com>
2025-04-03 16:57:28 -0400

    Practice file added' git push origin main

---
+++ Practice.sql
@@ -0,0 +1,13 @@
+use college;
+SELECT name, marks from STUDENT;
+
+Select * from student;
+
+SeLECT DISTINCt CITY FROM student;
+
+SELECT * FROM student where marks > 80;
+SELECT * FROM student where city = 'mumbai';
+
+SELECT * FROM student where marks > 80 and city = 'mumbai';
+
+SELECT * FROM student where marks+10 > 100;
